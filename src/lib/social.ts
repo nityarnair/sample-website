c361c6207c0cf6b29d6d38911b0e1b215e338bdb
Blessen P Shaju <blessenpshaju@gmail.com>
2025-05-26 11:10:30 +0530

    feat: add metadata constants for social card generators

---
+++ src/lib/social.ts
@@ -0,0 +1,5 @@
+export const SOCIAL_LINKS = {
+  rajagiriPortal: 'http://rcss.rajagiri.edu/',
+  dyutiEmail: 'mailto:dyuti@rajagiri.edu',
+  registrationForm: 'https://forms.gle/XTZZmXS1tjkvfm9u6',
+} as const;
